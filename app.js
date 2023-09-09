4e47a8f86d161a409066416856b43b10300e1405
Shahrukh-Rehman <shahrukhrehman321@gmail.com>
2023-09-10 00:27:16 +0500

    Question 1 to 4

---
+++ app.js
@@ -0,0 +1,16 @@
+//1
+// var age=26
+// alert("I am " + age + " years old")
+//2
+// var track= 15
+// alert("you have visited " + track + " times")
+//3
+// var birthYear=1996
+// document.write("My birth year is " + birthYear + "\nData type of my declared variable is number" )
+//4
+// var vistorsName= "John Doe"
+// var productTitle=" T-shirt(s)"
+// var quantity=5
+// document.write(vistorsName + " ordered " +quantity + productTitle + " on XYZ Clothing store")
+
+
